256aa9a55a45a88d70d083876350c64f78bf4ef1
thepromind <thepromind@gmail.com>
2020-02-06 15:58:05 +0600

    Edit Daniyar 07-02-2020

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Grid, Typography } from "@material-ui/core";
 import {
-    makeStyles,
-    createStyles,
-    Theme,
-    useTheme
+  makeStyles,
+  createStyles,
+  Theme,
+  useTheme
 } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import TextField from "@material-ui/core/TextField";
@@ -14,188 +14,200 @@ import Button from "@material-ui/core/Button";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 import ReactGA from "react-ga";
 import api from "../api/Api";
-import { paddingDownSm, rootSmXl } from './helper/DefaultStyle';
+import { paddingDownSm, rootSmXl } from "./helper/DefaultStyle";
 import MaskedInput from "react-maskedinput";
 import ym from "react-yandex-metrika";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 
 const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        [theme.breakpoints.down('sm')]: {
-            mainRoot: {
-                backgroundColor: 'white'
-            },
-            root: {
-                padding: paddingDownSm
-            },
-            title: {
-                fontStyle: "normal",
-                fontWeight: "bold",
-                fontSize: "26px",
-                marginBottom: 20
-            },
-            subtitle: {
-                fontStyle: "normal",
-                fontWeight: "bold",
-                fontSize: "18px",
-                color: "#5B5B5B",
-                marginBottom: 10
-            },
-            subtitleDesc: {
-                fontStyle: "normal",
-                fontWeight: "normal",
-                fontSize: "14px",
-                color: "#5B5B5B",
-                marginBottom: 10
-            },
-            buttonPartner: {
-                fontStyle: "normal",
-                fontWeight: "normal",
-                fontSize: 10,
-                color: "#5B5B5B",
-                marginTop: 10
-            },
-            noteButtonPartner: {
-                textTransform: "none",
-                color: "#3F0259",
-                fontSize: 14,
-                marginTop: 10,
-                paddingLeft: 20,
-                paddingRight: 20,
-                borderColor: "#3F0259"
-            }
-        },
-        [theme.breakpoints.between('sm', 'xl')]: {
-            mainRoot: {
-                padding: "20px 0"
-            },
-            ...rootSmXl,
-            mainBg: {
-                backgroundImage: 'url(partners_bg.svg)',
-                backgroundSize: 'contain',
-                backgroundPosition: 'right',
-                backgroundRepeat: 'no-repeat',
-            },
-            myFont: {
-                fontStyle: 'normal',
-                fontWeight: 'normal',
-                fontSize: 32,
-                textAlign: "center",
-                color: 'white'
-            },
-            title: {
-                fontStyle: "normal",
-                fontWeight: "bold",
-                fontSize: "40px",
-                marginBottom: 20
-            },
-            subtitle: {
-                fontStyle: "normal",
-                fontWeight: "bold",
-                fontSize: "20px",
-                color: "#5B5B5B",
-                marginBottom: 10
-            },
-            subtitleDesc: {
-                fontStyle: "normal",
-                fontWeight: "normal",
-                fontSize: "18px",
-                color: "#5B5B5B",
-                marginBottom: 10
-            },
-            buttonPartner: {
-                fontStyle: "normal",
-                fontWeight: "normal",
-                fontSize: 10,
-                color: "#5B5B5B",
-                marginTop: 10
-            },
-            noteButtonPartner: {
-                textTransform: "none",
-                color: "#3F0259",
-                fontSize: 16,
-                marginTop: 10,
-                paddingLeft: 20,
-                paddingRight: 20,
-                borderColor: "#3F0259"
-            }
-        }
-    })
-)
+  createStyles({
+    [theme.breakpoints.down("sm")]: {
+      mainRoot: {
+        backgroundColor: "white"
+      },
+      root: {
+        padding: paddingDownSm
+      },
+      title: {
+        fontStyle: "normal",
+        fontWeight: "bold",
+        fontSize: "26px",
+        marginBottom: 20
+      },
+      subtitle: {
+        fontStyle: "normal",
+        fontWeight: "bold",
+        fontSize: "18px",
+        color: "#5B5B5B",
+        marginBottom: 10
+      },
+      subtitleDesc: {
+        fontStyle: "normal",
+        fontWeight: "normal",
+        fontSize: "14px",
+        color: "#5B5B5B",
+        marginBottom: 10
+      },
+      buttonPartner: {
+        fontStyle: "normal",
+        fontWeight: "normal",
+        fontSize: 10,
+        color: "#5B5B5B",
+        marginTop: 10
+      },
+      noteButtonPartner: {
+        textTransform: "none",
+        color: "#3F0259",
+        fontSize: 14,
+        marginTop: 10,
+        paddingLeft: 20,
+        paddingRight: 20,
+        borderColor: "#3F0259"
+      }
+    },
+    [theme.breakpoints.between("sm", "xl")]: {
+      mainRoot: {
+        padding: "20px 0"
+      },
+      ...rootSmXl,
+      mainBg: {
+        backgroundImage: "url(partners_bg.svg)",
+        backgroundSize: "contain",
+        backgroundPosition: "right",
+        backgroundRepeat: "no-repeat"
+      },
+      myFont: {
+        fontStyle: "normal",
+        fontWeight: "normal",
+        fontSize: 32,
+        textAlign: "center",
+        color: "white"
+      },
+      title: {
+        fontStyle: "normal",
+        fontWeight: "bold",
+        fontSize: "40px",
+        marginBottom: 20
+      },
+      subtitle: {
+        fontStyle: "normal",
+        fontWeight: "bold",
+        fontSize: "20px",
+        color: "#5B5B5B",
+        marginBottom: 10
+      },
+      subtitleDesc: {
+        fontStyle: "normal",
+        fontWeight: "normal",
+        fontSize: "18px",
+        color: "#5B5B5B",
+        marginBottom: 10
+      },
+      buttonPartner: {
+        fontStyle: "normal",
+        fontWeight: "normal",
+        fontSize: 10,
+        color: "#5B5B5B",
+        marginTop: 10
+      },
+      noteButtonPartner: {
+        textTransform: "none",
+        color: "#3F0259",
+        fontSize: 16,
+        marginTop: 10,
+        paddingLeft: 20,
+        paddingRight: 20,
+        borderColor: "#3F0259"
+      }
+    }
+  })
+);
 interface TextMaskCustomProps {
-    inputRef: (ref: HTMLInputElement | null) => void;
+  inputRef: (ref: HTMLInputElement | null) => void;
 }
 const TextMaskCustom = (props: TextMaskCustomProps) => {
-    const { inputRef, ...other } = props;
+  const { inputRef, ...other } = props;
 
-    return (
-        <MaskedInput
-            {...other}
-            ref={(ref: any) => {
-                inputRef(ref ? ref.inputElement : null);
-            }}
-            mask="1(111) 111 11 11"
-            placeholder={"7(707) 707 77 77"}
-        />
-    );
+  return (
+    <MaskedInput
+      {...other}
+      ref={(ref: any) => {
+        inputRef(ref ? ref.inputElement : null);
+      }}
+      mask="1(111) 111 11 11"
+      placeholder={"7(707) 707 77 77"}
+    />
+  );
 };
 
 const Partners = (props: any) => {
-    const [fio, setFio] = React.useState("");
-    const [phoneNumber, setPhoneNumber] = React.useState("");
-    const [agree, setAgree] = React.useState<boolean>(true);
+  const [fio, setFio] = React.useState("");
+  const [phoneNumber, setPhoneNumber] = React.useState("");
+  const [agree, setAgree] = React.useState<boolean>(true);
 
-    const handleSubmit = (e: any) => {
-        e.preventDefault();
-        ReactGA.event({
-            category: "BccCard_kartakarta_Apply_Success",
-            action: "kartakarta_Apply_Success"
-        });
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    ReactGA.event({
+      category: "BccCard_kartakarta_Apply_Success",
+      action: "kartakarta_Apply_Success"
+    });
 
-        if (phoneNumber && setPhoneNumber) {
-            api.card
-                .order({ fio, phoneNumber })
-                .then(m => {
-                    setFio("");
-                    setPhoneNumber("");
-                    props.send();
-                })
-                .catch(e => console.warn(e));
-        }
-        ym("reachGoal", "send_mess");
-    };
+    if (phoneNumber && setPhoneNumber) {
+      api.card
+        .order({ fio, phoneNumber })
+        .then(m => {
+          setFio("");
+          setPhoneNumber("");
+          props.send();
+        })
+        .catch(e => console.warn(e));
+    }
+    ym("reachGoal", "send_mess");
+  };
 
+  const classes = useStyles({});
+  const { t } = useTranslation();
+  const theme = useTheme();
+  const isXS = useMediaQuery(theme.breakpoints.down("sm"));
 
-    const classes = useStyles({});
-    const { t } = useTranslation();
-    const theme = useTheme();
-    const isXS = useMediaQuery(theme.breakpoints.down("sm"));
+  const isValid = () =>
+    fio.length > 1 && phoneNumber.replace("_", "").length === 16 && agree;
 
-    const isValid = () =>
-        fio.length > 1 && phoneNumber.replace("_", "").length === 16 && agree;
+  return (
+    <Grid container className={classes.mainRoot}>
+      <Grid
+        container
+        className={`${classes.root} ${classes.mainBg}`}
+        spacing={4}
+      >
+        <Grid item xl={6} lg={6} md={6} sm={12} xs={12}>
+          <Typography className={classes.title}>
+            {t("partner.text_1")}
+          </Typography>
+          <Typography className={classes.subtitle}>
+            {t("partner.text_2")}
+          </Typography>
+          <Typography className={classes.subtitleDesc}>
+            {t("partner.text_3")}
+          </Typography>
+          <Typography className={classes.subtitleDesc}>
+            {t("partner.text_4")}
+          </Typography>
 
-    return (
-        <Grid container className={classes.mainRoot}>
-            <Grid container className={`${classes.root} ${classes.mainBg}`} spacing={4}>
-                <Grid item xl={6} lg={6} md={6} sm={12} xs={12}>
-                    <Typography className={classes.title}>{t('partner.text_1')}</Typography>
-                    <Typography className={classes.subtitle}>{t('partner.text_2')}</Typography>
-                    <Typography className={classes.subtitleDesc}>{t('partner.text_3')}</Typography>
-                    <Typography className={classes.subtitleDesc}>{t('partner.text_4')}</Typography>
-
-                    <Typography className={classes.buttonPartner}>
-                        <Button href="https://www.bcc.kz/partners/" target="_blank"
-                            variant="outlined"
-                            className={classes.noteButtonPartner}
-                        >
-                            {t('partner.button_main')}
-                        </Button>
-                    </Typography>
-                </Grid>
-
-            </Grid>
+          <Typography className={classes.buttonPartner}>
+            <Button
+              href="https://www.bcc.kz/partners/?utm_source=partners&utm_medium=button_click&utm_campaign=kartakarta."
+              target="_blank"
+              variant="outlined"
+              className={classes.noteButtonPartner}
+            >
+              {t("partner.button_main")}
+            </Button>
+          </Typography>
         </Grid>
-    )
-}
+      </Grid>
+    </Grid>
+  );
+};
 
 export default Partners;
